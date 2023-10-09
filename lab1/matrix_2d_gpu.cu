c4227dad6ca61301f36f1f851b88a1c5028bb4c5
AbderrahmaneAit gueni ssaid <aaitguenissaid@gmail.com>
2023-10-09 12:31:20 +0200

    corrections

--- lab1/matrix_2d_gpu.cu
+++ lab1/matrix_2d_gpu.cu
@@ -14,50 +14,52 @@ int main() {
     const int cols = N;
     const int blocksize = 4;
 
-    float **a_h = new float*[rows];
-    float **b_h = new float*[rows];
-    float **c_h = new float*[rows];
-    float **a_d;
-    float **b_d;
-    float **c_d;
+    float **a_h = new float *[rows];
+    float **b_h = new float *[rows];
+    float **c_h = new float *[rows];
 
     for (int i = 0; i < cols; i++) {
-	a_h[i] = new float[rows];
+        a_h[i] = new float[rows];
         b_h[i] = new float[rows];
         b_h[i] = new float[rows];
     }
 
-   for (int i = 0; i < rows; i++) {
+    float **a_d;
+    float **b_d;
+    float **c_d;
+
+    const int rows_size = rows * sizeof(float);
+    const int cols_size = cols * sizeof(float);
+
+    cudaMalloc((void **) &a_d, rows_size);
+    cudaMalloc((void **) &b_d, rows_size);
+    cudaMalloc((void **) &c_d, rows_size);
+
+    for (int i = 0; i < rows; i++) {
+        cudaMalloc((void **) &a_d[i], cols_size);
+        cudaMalloc((void **) &b_d[i], cols_size);
+        cudaMalloc((void **) &c_d[i], cols_size);
+    }
+
+    for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             a_h[i][j] = 10 + i;
             b_h[i][j] = float(j) / N;
         }
     }
 
-    const int rows_size = N * sizeof(float);
-    const int cols_size = N * sizeof(float);
-    cudaMalloc((void **) &a_d, rows_size);
-    cudaMalloc((void **) &b_d, rows_size);
-    cudaMalloc((void **) &c_d, rows_size);
-
-    for (int i = 0; i < N; i++) {
-        cudaMalloc((void **) &a_d[i], cols_size);
-        cudaMalloc((void **) &b_d[i], cols_size);
-        cudaMalloc((void **) &c_d[i], cols_size);
-    }
-
     cudaMemcpy2D(a_d[0], cols_size, a_h[0], cols_size, cols_size, rows, cudaMemcpyHostToDevice);
     cudaMemcpy2D(b_d[0], cols_size, b_h[0], cols_size, cols_size, rows, cudaMemcpyHostToDevice);
 
     dim3 dimBlock(blocksize, blocksize);
-    dim3 dimGrid(cols / dimBlock.x, cols / dimBlock.x);
+    dim3 dimGrid(rows / dimBlock.x, cols / dimBlock.y);
 
     add_matrix<<<dimGrid, dimBlock>>>(a_d, b_d, c_d);
 
     cudaMemcpy2D(c_h[0], cols_size, c_d[0], cols_size, cols_size, rows, cudaMemcpyDeviceToHost);
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             std::cout << c_h[i][j] << " ";
         }
         std::cout << std::endl;
