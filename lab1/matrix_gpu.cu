6bb08a37d43d30cea5842ecfcffffb0d2b7c2d08
AbderrahmaneAit gueni ssaid <aaitguenissaid@gmail.com>
2023-10-11 12:46:51 +0200

    updated gpu variable names

--- lab1/matrix_gpu.cu
+++ lab1/matrix_gpu.cu
@@ -11,18 +11,17 @@ int main() {
     const int N = 16;
     const int blocksize = 4;
 
-    float *a = new float[N * N];
-    float *b = new float[N * N];
+    float *a_h = new float[N * N];
+    float *b_h = new float[N * N];
     float *c_h = new float[N * N];
     float *a_d;
     float *b_d;
     float *c_d;
 
-
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++) {
-            a[i + j * N] = 10 + i;
-            b[i + j * N] = float(j) / N;
+            a_h[i + j * N] = 10 + i;
+            b_h[i + j * N] = float(j) / N;
         }
 
     const int size = N * N * sizeof(float);
@@ -30,8 +29,8 @@ int main() {
     cudaMalloc((void **) &b_d, size);
     cudaMalloc((void **) &c_d, size);
 
-    cudaMemcpy(a_d, a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(b_d, b, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(a_d, a_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(b_d, b_h, size, cudaMemcpyHostToDevice);
 
     dim3 dimBlock(blocksize, 1);
     dim3 dimGrid((N * N) / dimBlock.x, 1);
