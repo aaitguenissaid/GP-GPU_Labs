77c016d11175b49e30234f1f82391ed051e2e5cd
aaitguenissaid <aaitguenissaid@gmail.com>
2023-10-13 11:42:59 +0200

    2d matrix addition on one dimension array - it works

--- lab1/matrix_2d_gpu.cu
+++ lab1/matrix_2d_gpu.cu
@@ -2,74 +2,68 @@
 
 #include <iostream>
 
-__global__ void add_matrix(float **a, float **b, float **c) {
-    int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
-    int idx_y = blockIdx.y * blockDim.y + threadIdx.y;
-    c[idx_x][idx_y] = a[idx_x][idx_y] + b[idx_x][idx_y];
+__global__ void add_matrix(float *a, float *b, float *c, int rows, int cols) {
+    int i = blockIdx.y * blockDim.y + threadIdx.y; // row
+    int j = blockIdx.x * blockDim.x + threadIdx.x; // col
+    
+    if(i < rows && j < cols) {
+        int index = i * cols + j;
+        c[index] = a[index] + b[index];
+    }
 }
 
 int main() {
-    const int N = 16;
-    const int rows = N;
-    const int cols = N;
-    const int blocksize = 4;
+    const int rows = 8;
+    const int cols = 5;
+    const int blocksize = 16;
 
-    float **a_h = new float *[rows];
-    float **b_h = new float *[rows];
-    float **c_h = new float *[rows];
+    float *a_h = new float[rows * cols];
+    float *b_h = new float[rows * cols];
+    float *c_h = new float[rows * cols];
+    float *a_d;
+    float *b_d;
+    float *c_d;
 
-    for (int i = 0; i < cols; i++) {
-        a_h[i] = new float[rows];
-        b_h[i] = new float[rows];
-        b_h[i] = new float[rows];
-    }
-
-    float **a_d;
-    float **b_d;
-    float **c_d;
-
-    const int rows_size = rows * sizeof(float);
-    const int cols_size = cols * sizeof(float);
-
-    cudaMalloc((void **) &a_d, rows_size);
-    cudaMalloc((void **) &b_d, rows_size);
-    cudaMalloc((void **) &c_d, rows_size);
-
-    for (int i = 0; i < rows; i++) {
-        cudaMalloc((void **) &a_d[i], cols_size);
-        cudaMalloc((void **) &b_d[i], cols_size);
-        cudaMalloc((void **) &c_d[i], cols_size);
-    }
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++) {
+            a_h[i * cols + j] = i;
+            b_h[i * cols + j] = j;
+        }
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            a_h[i][j] = 10 + i;
-            b_h[i][j] = float(j) / N;
+            std::cout << a_h[i * cols + j ] << " ";
         }
+        std::cout << std::endl;
     }
+    std::cout << "\n" << std::endl;
 
-    size_t a_dev_pitch;
-    size_t b_dev_pitch;
-    size_t c_dev_pitch;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            std::cout << b_h[i * cols + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "\n" << std::endl;
 
-    cudaMallocPitch(&a_d[0], &a_dev_pitch, cols_size * sizeof(float), rows);
-    cudaMallocPitch(&b_d[0], &b_dev_pitch, cols_size * sizeof(float), rows);
+    const int size = rows * cols * sizeof(float);
+    cudaMalloc((void **) &a_d, size);
+    cudaMalloc((void **) &b_d, size);
+    cudaMalloc((void **) &c_d, size);
 
-    cudaMemcpy2D(a_d[0], cols_size, a_h[0], cols_size, cols_size, rows, cudaMemcpyHostToDevice);
-    cudaMemcpy2D(b_d[0], cols_size, b_h[0], cols_size, cols_size, rows, cudaMemcpyHostToDevice);
+    cudaMemcpy(a_d, a_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(b_d, b_h, size, cudaMemcpyHostToDevice);
 
     dim3 dimBlock(blocksize, blocksize);
-    dim3 dimGrid(rows / dimBlock.x, cols / dimBlock.y);
+    dim3 dimGrid((rows-1)/dimBlock.x + 1, ceil(float(cols)/dimBlock.y));
 
-    add_matrix<<<dimGrid, dimBlock>>>(a_d, b_d, c_d);
+    add_matrix<<<dimGrid, dimBlock>>>(a_d, b_d, c_d, rows, cols);
 
-    cudaMallocPitch(&c_h[0], &c_dev_pitch, cols_size * sizeof(float), rows);
-
-    cudaMemcpy2D(c_h[0], cols_size, c_d[0], cols_size, cols_size, rows, cudaMemcpyDeviceToHost);
+    cudaMemcpy(c_h, c_d, size, cudaMemcpyDeviceToHost);
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            std::cout << c_h[i][j] << " ";
+            std::cout << c_h[i * cols + j] << " ";
         }
         std::cout << std::endl;
     }
