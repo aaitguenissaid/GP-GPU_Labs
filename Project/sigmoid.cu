1d0a0ce1b2e0465086433c16aac2d1dde33e0d39
AbderrahmaneAit gueni ssaid <aaitguenissaid@gmail.com>
2023-11-13 17:08:55 +0100

    corrections

--- Project/sigmoid.cu
+++ Project/sigmoid.cu
@@ -10,19 +10,16 @@ __global__ void sigmoid_kernel(float *input, int rows, int cols) {
 
     if(i < rows && j < cols) {
         int index = i * cols + j;
-<<<<<<< HEAD
-        input[index] = 1/(1 + np.exp(-input[index]))
-=======
-        a[index] = 1.0 /(1.0 + expf(-a[index]));
->>>>>>> 73dce5baab5ffad71c84736c72ac14a437c4804b
+        input[index] = 1/(1 + expf(-input[index]))
     }
 }
 
 extern "C"
-void sigmoid_of_matrix(float *input, int rows, int cols) {
+float* sigmoid_of_matrix(float *input, int rows, int cols) {
     const int blocksize = 256;
     dim3 dimBlock(blocksize, blocksize);
     dim3 dimGrid((rows-1)/dimBlock.x + 1, ceil(float(cols)/dimBlock.y));
     sigmoid_kernel<<<dimGrid, dimBlock>>>(input, rows, cols);
     cudaDeviceSynchronize();  // Wait for the kernel to finish
+    return input
 }
